1200e661be59d106e11d012a76a4f9b2260a7b11
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2023-01-12 16:34:36 +0200

    Add files via upload

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -6,11 +6,21 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { OpenDialog, OfferProducts } from "./UserContext";
+import IconButton from "@mui/material/IconButton";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
 
 const Rating = (store) => {
   //useContext
   const { open, setOpen } = useContext(OpenDialog);
-  const { offerProducts, setOfferProducts } = useContext(OfferProducts);
+  const { offerProducts } = useContext(OfferProducts);
+
+  //useStates
+  const [Active, setActive] = useState(false);
+
+  const handleClickLike = () => {
+    setActive(!Active);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -20,6 +30,14 @@ const Rating = (store) => {
     setOpen(false);
   };
 
+  //useEffect//
+  useEffect(() => {
+  }, []);
+
+
+ 
+
+
   //format date //
   const getFormattedDate = (dateStr) => {
     const date = new Date(dateStr);
@@ -27,47 +45,58 @@ const Rating = (store) => {
   };
   return (
     <Fragment>
-          <div>
-            <Button size="small" variant="outlined" onClick={handleClickOpen}>
-              Αξιολόγηση{" "}
+      <div>
+        <Button size="small" variant="outlined" onClick={handleClickOpen}>
+          Αξιολόγηση{" "}
+        </Button>
+        <Dialog
+          maxWidth="400"
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {store.store.name}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <b>ΠΡΟΣΦΟΡΕΣ</b>
+              {offerProducts &&
+                offerProducts.map(
+                  (indexProduct) =>
+                    store.store.id === indexProduct.id && (
+                      <p>
+                        {indexProduct.product_name} {" Από "}
+                        {indexProduct.init_price} {"€ Μόνο "}
+                        {indexProduct.new_price} {"€ Ημερομηνία κατ: "}
+                        {getFormattedDate(indexProduct.entry_date)} {", Likes "}
+                        {indexProduct.likes} {", Dislikes "}
+                        {indexProduct.dislikes} {", Σε απόθεμα: "}
+                        {indexProduct.stock === true ? "ΝΑΙ" : "OXI"}
+                        <IconButton onClick={() => handleClickLike()}>
+                          {Active === false ? (
+                            <ThumbUpOutlinedIcon />
+                          ) : (
+                            <ThumbUpOutlinedIcon color={"success"} />
+                          )}
+                        </IconButton>
+                        <IconButton>
+                          <ThumbDownOffAltOutlinedIcon />
+                        </IconButton>
+                        <br />
+                      </p>
+                    )
+                )}
+              {console.log(store.store.id)}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="error" autoFocus onClick={handleClose}>
+              Έξοδος
             </Button>
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="responsive-dialog-title"
-            >
-              <DialogTitle id="responsive-dialog-title">
-                {store.store.name}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  {offerProducts &&
-                    offerProducts.map(
-                      (indexProduct) =>
-                        store.store.id === indexProduct.id && (
-                          <p>
-                            {indexProduct.product_name} {" Από "}
-                            {indexProduct.init_price} {"€ Μόνο "}
-                            {indexProduct.new_price} {"€ Ημερομηνία κατ: "}
-                            {getFormattedDate(indexProduct.entry_date)}{" "}
-                            {", Likes "}
-                            {indexProduct.likes} {", Dislikes "}
-                            {indexProduct.dislikes} {", Σε απόθεμα: "}
-                            {indexProduct.stock === true ? "ΝΑΙ" : "OXI"}
-                            <br />
-                          </p>
-                        )
-                    )}
-                    {console.log(store.store.id)}
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button color="error" autoFocus onClick={handleClose}>
-                  Έξοδος
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </div>
+          </DialogActions>
+        </Dialog>
+      </div>
     </Fragment>
   );
 };
