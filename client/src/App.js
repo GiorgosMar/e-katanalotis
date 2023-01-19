928650bafece179fcd1bef2dc66c816caaf7ded8
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2023-01-19 13:06:29 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,19 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 //Components
 import DashboardUser from "./components/DashboardUser";
 import DashboardAdmin from "./components/DashboardAdmin";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { UserContext } from "./components/UserContext";
+import { UserContext, UserCredentials } from "./components/UserContext";
 
 function App() {
   //useStates
+  const [userCredentials, setUserCredentials] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [role, setRole] = useState(null);
 
@@ -30,10 +33,10 @@ function App() {
 
       if (checkAuthenticated.auth === true) {
         setIsAuthenticated(true);
-        setRole(checkAuthenticated.user_credentials);
+        setRole(checkAuthenticated.user_credentials.user_role);
+        setUserCredentials(checkAuthenticated.user_credentials);
       } else {
         setIsAuthenticated(false);
-        setRole(checkAuthenticated.user_credentials);
       }
     } catch (err) {
       console.error(err.message);
@@ -43,90 +46,96 @@ function App() {
   //useEffects
   useEffect(() => {
     checkAuthenticated();
-  });
+  }, []);
 
   return (
     <Fragment>
-      <Router>
-        <UserContext.Provider
-          value={{
-            isAuthenticated,
-            setIsAuthenticated,
-            role,
-            setRole,
-          }}
-        >
-          <Routes>
-            <Route
-              index
-              path="/"
-              element={
-                !isAuthenticated ? (
-                  <Navigate to="/login" />
-                ) : isAuthenticated && role === 1 ? (
-                  <Navigate to="/dashboardAdmin" />
-                ) : isAuthenticated && role === 0 ? (
-                  <Navigate to="/dashboardUser" />
-                ) : (
-                  <Navigate to="/login" />
-                )
-              }
-            />
-            <Route
-              exact
-              path="/login"
-              element={
-                !isAuthenticated ? (
-                  <Login />
-                ) : isAuthenticated && role === 1 ? (
-                  <Navigate to="/dashboardAdmin" />
-                ) : isAuthenticated && role === 0 ? (
-                  <Navigate to="/dashboardUser" />
-                ) : (
-                  <Navigate to="/login" />
-                )
-              }
-            />
-            <Route
-              exact
-              path="/register"
-              element={
-                !isAuthenticated ? <Register /> : <Navigate to="/login" />
-              }
-            />
-            <Route
-              exact
-              path="/dashboardUser"
-              element={
-                !isAuthenticated ? (
-                  <Navigate to="/login" />
-                ) : isAuthenticated && role === 1 ? (
-                  <Navigate to="/dashboardAdmin" />
-                ) : isAuthenticated && role === 0 ? (
-                  <DashboardUser />
-                ) : (
-                  <Navigate to="/login" />
-                )
-              }
-            />
-            <Route
-              exact
-              path="/dashboardAdmin"
-              element={
-                !isAuthenticated ? (
-                  <Navigate to="/login" />
-                ) : isAuthenticated && role === 1 ? (
-                  <DashboardAdmin />
-                ) : isAuthenticated && role === 0 ? (
-                  <Navigate to="/dashboardUser" />
-                ) : (
-                  <Navigate to="/login" />
-                )
-              }
-            />
-          </Routes>
-        </UserContext.Provider>
-      </Router>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Router>
+          <UserCredentials.Provider
+            value={{ userCredentials, setUserCredentials }}
+          >
+            <UserContext.Provider
+              value={{
+                isAuthenticated,
+                setIsAuthenticated,
+                role,
+                setRole,
+              }}
+            >
+              <Routes>
+                <Route
+                  index
+                  path="/"
+                  element={
+                    !isAuthenticated ? (
+                      <Navigate to="/login" />
+                    ) : isAuthenticated && role === 1 ? (
+                      <Navigate to="/dashboardAdmin" />
+                    ) : isAuthenticated && role === 0 ? (
+                      <Navigate to="/dashboardUser" />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route
+                  exact
+                  path="/login"
+                  element={
+                    !isAuthenticated ? (
+                      <Login />
+                    ) : isAuthenticated && role === 1 ? (
+                      <Navigate to="/dashboardAdmin" />
+                    ) : isAuthenticated && role === 0 ? (
+                      <Navigate to="/dashboardUser" />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route
+                  exact
+                  path="/register"
+                  element={
+                    !isAuthenticated ? <Register /> : <Navigate to="/login" />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboardUser"
+                  element={
+                    !isAuthenticated ? (
+                      <Navigate to="/login" />
+                    ) : isAuthenticated && role === 1 ? (
+                      <Navigate to="/dashboardAdmin" />
+                    ) : isAuthenticated && role === 0 ? (
+                      <DashboardUser />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboardAdmin"
+                  element={
+                    !isAuthenticated ? (
+                      <Navigate to="/login" />
+                    ) : isAuthenticated && role === 1 ? (
+                      <DashboardAdmin />
+                    ) : isAuthenticated && role === 0 ? (
+                      <Navigate to="/dashboardUser" />
+                    ) : (
+                      <Navigate to="/login" />
+                    )
+                  }
+                />
+              </Routes>
+            </UserContext.Provider>
+          </UserCredentials.Provider>
+        </Router>
+      </LocalizationProvider>
     </Fragment>
   );
 }
