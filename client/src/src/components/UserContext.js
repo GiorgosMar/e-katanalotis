3014707303a6b052a5ba2c80958408176ace3187
Georgios Marinos <95182300+GiorgosMar@users.noreply.github.com>
2023-02-24 14:50:39 +0200

    Delete client/src/src directory

--- client/src/src/components/UserContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext(null);
-export const UserPosition = createContext(null);
-export const OpenDialog = createContext(null);
-export const OfferProducts = createContext([]);
-export const UserCredentials = createContext([]);
-export const ReactedProducts = createContext([]);
-
